92acea366e37ff8bf173d1ec6fbe5dc3b0bf50ab
luis-tevasamorir <87385540+luis-tevasamorir@users.noreply.github.com>
2023-10-06 22:18:26 -0600

    test.js

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
 <script type="text/javascript">
-    // Este es un script de Hola Mundo en JavaScript
-    console.log("¡Hola Mundo!");
-</script>
+         // Obtenemos el elemento de texto por su ID
+         var elementoTexto = document.getElementById("mensaje");
+
+         // Creamos el mensaje "Hola Mundo"
+         var mensaje = "¡Hola Mundo!";
+
+         // Asignamos el mensaje al contenido del elemento de texto
+         elementoTexto.textContent = mensaje;
+      </script>
