34069d9461a68341cb5fe4859639f7e6deb6bbed
luis-tevasamorir <87385540+luis-tevasamorir@users.noreply.github.com>
2024-03-25 16:35:17 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 <script type="text/javascript">
          // Obtenemos el elemento de texto por su ID
          var elementoTexto = document.getElementById("mensaje");
-
+         console.log('¡Hola Mundo!'))
          // Creamos el mensaje "Hola Mundo"
          var mensaje = "¡Hola Mundo!";
 
